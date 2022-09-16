d08a02fb1876d9f18b9ec118f3d77a9f75d8fd4e
usman-maqbool <engusman8279@gmail.com>
2022-09-16 16:49:39 +0500

    change app name

--- predoc/urls.py
+++ predoc/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('core.urls')),
+    path('',include('appoinments.urls')), 
     
     path('account/',include('account.urls')),
 ]

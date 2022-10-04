753c5bf27034f888f4fee6e898838f977d03bfb2
usman-maqbool <engusman8279@gmail.com>
2022-10-04 19:18:31 +0500

    adding and clean code

--- appoinments/views.py
+++ appoinments/views.py
@@ -21,10 +21,12 @@ class ContactUsPageView(View):
 class DashBoardPageView(LoginRequiredMixin,View):
     def get(self,request):
         appoinments = Appoinment.objects.all()
+        obj = Appoinment.objects.first()
 
         context={
             "title":"Dashboard",
             "appoinments":appoinments,
+            "obj":obj
         }
         return render(request,'dashboard.html', context)
 
@@ -70,21 +72,10 @@ class DisAgrePageView(LoginRequiredMixin,View):
         }
         return render(request, 'pages/disagree.html', context)
 
-# class AllPatientView(View):
-#     def get(self,request, *args, **kwargs):
-#         # appoinments = Appoinment.objects.all()
-#         id = kwargs.get('id')
-#         appoinments = get_object_or_404(Appoinment, id=id)
-#         print(id,'id is')
-#         context={
-#             "appoinments":appoinments,
-#         }
-#         return render(request,'pages/all_patiebt.html', context)
 
 
 class AllPatientView(View):
     def get(self,request, *args, **kwargs):
-        # appoinments = Appoinment.objects.all()
         id = kwargs.get('id')
         appoinments = get_object_or_404(Appoinment, id=id)
         context={
--- static/assets/js/dashboard.js
+++ static/assets/js/dashboard.js
@@ -32,26 +32,13 @@
   $('#myTable').DataTable();
 } );
 
-    // Model Show Load
+  // Model Show Load
   $(document).ready(function(){
       $("#myModal").modal('show');
   });
 
-  // User Details Models 
-  var elements = document.querySelectorAll('.show_div')
-  for (let element of elements ){
-    let expandbtn = element.querySelector('.button_show_div')
-    expandbtn.addEventListener('click', (e) =>{
-      setTimeout(function() {
-        $('.show_able_div').removeClass('hide');
-        }, 2000);
-      });
-      $('.button_show_div').on("click",function(){
-        $(window).scrollTop(0);
-    });
-  }
 
-      //  Spinner
+  //  Spinner
   function loadingPage() {
     var div = document.getElementById("loading-wrapper")
     div.style.display = "block";
@@ -60,3 +47,31 @@
   }, 
   2000);
 }
+// axios request
+let get_ajax_request = document.querySelectorAll('.button_show_div')
+var show_data = document.getElementById('show_able_div')
+for (let element of get_ajax_request){
+  element.addEventListener('click',(e) => {
+    setTimeout(function() {
+    // spinner timeout
+      $('.show_able_div').removeClass('hide');
+      }, 2000);
+    // scroll top
+      $('.button_show_div').on("click",function(){
+        $(window).scrollTop(0);
+    });
+    // axios request
+    id = element.getAttribute('data-target').slice(4)
+    axios.get('/all-patient/' +id).then(function(resp){
+      show_data.innerHTML = resp.data
+    }).catch(function(err){
+      console.log(err)
+    })
+    // Hide current div
+    var current_div = document.getElementById('current_div')
+    if( current_div.style.display = 'block'){
+      current_div.style.display = 'none'
+    }
+  })
+}    
+   
